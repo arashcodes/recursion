9bd1b82c4f887d23aec88720ff0a06fd1656fdc9
Arash <arashabbasi@Arashs-MacBook-Pro.local>
2019-10-03 11:18:03 -0700

    bare min completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var everything = document.body;
+  var outputArray = [];
+  var recursiveCheck = function(nodeList) {
+     if (nodeList.classList !== undefined && nodeList.classList.contains(className)) {
+      outputArray.push(nodeList)
+    }
+    if (nodeList.hasChildNodes()) {
+      for (var i = 0; i < nodeList.childNodes.length; i++) {
+        recursiveCheck(nodeList.childNodes[i]);
+      }
+    }
+  }
+  recursiveCheck(everything);
+  return outputArray;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,32 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var resultArr = [];
+
+	if ((obj === null) || (typeof obj === "number") || (typeof obj === "boolean")) {
+		return '' + obj;
+	}
+	if (typeof obj === "string") {
+		return '"' + obj + '"';
+	} else if (Array.isArray(obj)) {
+		for ( var i = 0; i < obj.length; ++i) {
+			if ((typeof obj[i] === "undefined") || (obj[i] === null) || (typeof obj[i] === "function") || (Number.isNaN(obj[i]))) {
+				return 'null';
+			} else {
+		    	resultArr.push(stringifyJSON(obj[i]));
+			};
+	    }
+
+	    return '[' + resultArr + ']'
+
+	 } else if (typeof obj === 'object') {
+	 	for (var key in obj) {
+	 		if (typeof obj[key] === "undefined" || typeof obj[key] === 'function' || typeof obj[key] === 'symbol') {
+              return obj[key] = '{}';
+	 	}   else {
+	 		resultArr.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+	 	}
+	 }
+	 	return '{' + resultArr + '}'
+   }
 };
